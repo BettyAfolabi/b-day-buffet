71bfea0fade135174700a08a02e0b45ed8f20117
Betty <betty4web@gmail.com>
2026-03-27 15:52:12 +0100

    update slot

--- app/src/lib/constants.ts
+++ app/src/lib/constants.ts
@@ -116,7 +116,7 @@ export const QUESTION_FLOW = {
 
 export const SLOTS = {
   virtual: [
-    { id: "v2", label: "Mon, March 30 @ 8:00 PM"  },
+    { id: "v1", label: "Mon, March 30 @ 2:00 PM" },
     { id: "v4", label: "Thu, April 2 @ 8:00 PM"  },
     { id: "v5", label: "Mon, April 6 @ 2:00 PM"  },
   ],
